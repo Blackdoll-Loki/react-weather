a6186d45e4f24583c6bbc4f26204a8ed57841581
Blackdoll-Loki <helpmeloki7@gmail.com>
2023-03-07 07:21:22 +0200

    add search component and list component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SearchEngine from "./SearchEngine";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>Weather App</h1>
+      <SearchEngine />
+      <footer>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <a
+            href="https://github.com/Blackdoll-Loki/react-weather"
+            target="_blank"
+          >
+            Open source-code
+          </a>
+          by Daria
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </footer>
     </div>
   );
 }
---
+++ src/SearchEngine.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+/*import axios from "axios";*/
+import List from "./List";
+
+export default function SearchEngine() {
+  let [temperature, setTemperature] = useState(null);
+  let [city, setCity] = useState("");
+  function getCity(event) {
+    setCity(event.target.value);
+  }
+  function storeWeather(response) {
+    let arr = makeWeatherArr(response);
+    setTemperature(arr);
+  }
+  function makeWeatherArr(obj) {
+    let arr = [].concat(
+      Math.round(obj.temperature.current),
+      obj.condition.description,
+      obj.temperature.humidity,
+      obj.wind.speed,
+      obj.condition.icon_url
+    );
+    return arr;
+  }
+  async function getWeather(event) {
+    event.preventDefault();
+    let apiKey = `be31ee9ff95t7734bo02a1e16b490b16`;
+    let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    /*axios.get(url).then(storeWeather);*/
+    fetch(url)
+      .then((response) => response.json())
+      .then((obj) => {
+        console.log(obj);
+        storeWeather(obj);
+      });
+  }
+  if (temperature) {
+    return (
+      <div className="App">
+        <form onSubmit={getWeather}>
+          <input type="text" placeholder="Enter a city..." onChange={getCity} />
+          <input type="button" value="Submit" />
+        </form>
+        <List temperature={temperature} />
+      </div>
+    );
+  } else {
+    return (
+      <form onSubmit={getWeather} className="App">
+        <input type="text" placeholder="Enter a city..." onChange={getCity} />
+        <input type="button" value="Submit" />={" "}
+      </form>
+    );
+  }
+}
